3ee7828d2e0c18e82917ae0163ed937519fab42c
carolyne <pcmaunda@gmail.com>
2022-02-13 15:50:39 +0300

    added observable

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GitsearchServicesService } from '../service/gitsearch-services.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private_services:GitsearchServicesService) { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/service/gitsearch-services.service.ts
+++ src/app/service/gitsearch-services.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitsearchServicesService {
+  _http: any;
 
-  constructor() { }
+  constructor(private_http:HttpClient)   { }
+   gitsearchUrl = "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"
+  Navbar():observable<any>
+{
+return this._http.get(this.gitsearchUrl)
+}
 }
